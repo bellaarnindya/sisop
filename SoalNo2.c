7fb233c925666b84686daa4b798a325230c56ae6
bellaarnindya <bellaarnindya@gmail.com>
2015-11-01 15:31:23 +0700

    Soal no 2, bilangan prima

---
+++ SoalNo2.c
@@ -0,0 +1,33 @@
+
+#include<stdio.h>
+#include<pthread.h>
+int total=0, sum=0;
+void *prima (void *args)
+{
+	int *i, n, j, factor;
+	factor=0;
+	i = (int*)args;
+	for (j=1; j<=*i; j++)
+	{
+		if (*i%j==0) factor++;
+	}
+	if (factor==2)
+	{
+		sum+=*i;
+		total++;
+	}
+}
+
+void main()
+{
+	pthread_t t1;
+	int i, n;
+	scanf("%d", &n);
+	for (i=2; i<n; i++)
+	{
+		pthread_create(&t1, NULL, prima, (void*)&i);
+		pthread_join(t1, NULL);
+	}
+	printf("Jumlah bilangan prima: %d\n", sum);
+	printf("Banyak bilangan prima: %d\n", total);
+}
